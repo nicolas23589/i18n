15924dd843ab24d28d92c16a5f9996ebe1683245
Nicolás23589 <n.murilloc@uniandes.edu.co>
2024-09-29 23:54:12 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobslist";
-
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import localeEsMessages from "./locales/es";
+ReactDOM.render(
+    <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+            <JobsList/>
+    </IntlProvider>, document.getElementById("root"));
